ed6257bfff1db63b9ea1f3fb13f8b89a9fd010c4
taguru <mohammed.taj@halan.com>
2023-11-06 23:05:50 +0200

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,111 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.example.mytask"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mytask"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.5.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha01")
+
+    // Image loading
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    // Hilt
+//    implementation("com.google.dagger:dagger:2.44")
+//    kapt("com.google.dagger:dagger-compiler:2.44")
+//    api("com.google.dagger:dagger-android:2.37")
+//    api("com.google.dagger:dagger-android-support:2.37")
+//    kapt("com.google.dagger:dagger-android-processor:2.37")
+//    implementation("com.google.dagger:hilt-android:2.44")
+//    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+//    kapt("com.google.dagger:hilt-compiler:2.44")
+//    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    implementation( "com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-compiler:2.44")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+   /* api ("com.google.dagger:dagger-android:2.37")
+    api ("com.google.dagger:dagger-android-support:2.37")
+    kapt( "com.google.dagger:dagger-android-processor:2.37")
+    implementation( "com.google.dagger:hilt-android:2.38.1")
+    kapt( "com.google.dagger:hilt-android-compiler:2.38.1")*/
+
+   // kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.4.2")
+}
+
+kapt {
+    correctErrorTypes = true
+}
